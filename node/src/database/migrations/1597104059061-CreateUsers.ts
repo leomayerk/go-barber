220b3f1f7393484de2b69bde548dee0220a04548
unknown <leomayerk@hotmail.com>
2020-08-10 23:36:59 -0300

    Add: User model and migration

--- node/src/database/migrations/1597104059061-CreateUsers.ts
+++ node/src/database/migrations/1597104059061-CreateUsers.ts
@@ -1,11 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1595463367353
-  implements MigrationInterface {
+export default class CreateUsers1597104059061 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -15,22 +14,27 @@ export default class CreateAppointments1595463367353
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone', // anothers bd's is only 'timestamp'
+            name: 'email',
+            type: 'varchar', // anothers bd's is only 'timestamp'
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
           },
           {
             name: 'created_at',
             type: 'timestamp',
-            default: 'now()',
+            default: 'now()'
           },
           {
             name: 'updated_at',
             type: 'timestamp',
-            default: 'now()',
+            default: 'now()'
           },
         ],
       })
@@ -38,6 +42,6 @@ export default class CreateAppointments1595463367353
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
