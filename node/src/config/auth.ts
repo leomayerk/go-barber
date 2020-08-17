93616ad9030ac1f12bf135d614e6a41686ff7adb
unknown <leomayerk@hotmail.com>
2020-08-16 21:48:48 -0300

    Add: Authenticated routes

---
+++ node/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '4fd07db48d97fae3e8f91c25110a0019',
+    expiresIn: '1d',
+  }
+}
--- node/src/routes/appointments.routes.ts
+++ node/src/routes/appointments.routes.ts
@@ -4,36 +4,45 @@ import { getCustomRepository } from 'typeorm';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
+
+appointmentsRouter.use(ensureAuthenticated);
 
 //SoC: Separation of Concerns (separação de preocupações)
 // por isso existe o Repository separado do routes e do model
 
-appointmentsRouter.get('/', async (request, response) => {
-  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-  const appointments = await appointmentsRepository.find();
+appointmentsRouter.get(
+  '/',
+  /*ensureAuthenticated,*/ async (request, response) => {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+    const appointments = await appointmentsRepository.find();
 
-  return response.json(appointments);
-});
-
-appointmentsRouter.post('/', async (request, response) => {
-  try {
-    const { provider_id, date } = request.body;
-
-    const parsedDate = parseISO(date);
-
-    const createAppointment = new CreateAppointmentService();
-
-    const appointment = await createAppointment.execute({
-      provider_id,
-      date: parsedDate,
-    });
-
-    return response.json(appointment);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
+    return response.json(appointments);
   }
-});
+);
+
+appointmentsRouter.post(
+  '/',
+  /*ensureAuthenticated,*/ async (request, response) => {
+    try {
+      const { provider_id, date } = request.body;
+
+      const parsedDate = parseISO(date);
+
+      const createAppointment = new CreateAppointmentService();
+
+      const appointment = await createAppointment.execute({
+        provider_id,
+        date: parsedDate,
+      });
+
+      return response.json(appointment);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  }
+);
 
 export default appointmentsRouter;
