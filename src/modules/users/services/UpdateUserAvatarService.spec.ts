2a9d57d8f7b21204725f10407023c5ff3eadc738
Leonardo Mayer Kleesattel <leomayerk@hotmail.com>
2021-08-01 12:25:33 -0300

    Password recover

--- src/modules/users/services/UpdateUserAvatarService.spec.ts
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -30,7 +30,7 @@ describe('UpdateUserAvatar', () => {
 
         const updateUserAvatar = new UpdateUserAvatarService(fakeUsersRepository, fakeStorageProvider);
 
-        expect(
+        await expect(
             updateUserAvatar.execute({
                 user_id: 'non-existing-user',
                 avatarFileName: 'avatar.jpg'
