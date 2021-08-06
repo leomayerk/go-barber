4c901873330ef2c88152daa5b7fe454edb17491a
Leonardo Mayer Kleesattel <leomayerk@hotmail.com>
2021-08-06 11:56:21 -0300

    Providers List

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 
-
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
 
