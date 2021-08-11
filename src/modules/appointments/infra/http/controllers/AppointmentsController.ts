b56d819a9cded7d6f961dd3548d04156b530eba2
Leonardo Mayer Kleesattel <leomayerk@hotmail.com>
2021-08-11 17:46:14 -0300

    Provider schedule

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -5,7 +5,7 @@ import { container } from 'tsyringe';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 export default class AppointmentsController {
-    async create(request: Request, response: Response): Promise<Response> {
+    public async create(request: Request, response: Response): Promise<Response> {
         const user_id = request.user.id;
         const { provider_id, date } = request.body;
 
