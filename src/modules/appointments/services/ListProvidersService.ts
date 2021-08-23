a29f1d430c511a313ce118c8d73870041168c48d
Leonardo Mayer Kleesattel <leomayerk@hotmail.com>
2021-08-23 14:48:22 -0300

    Api adjusts

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -19,16 +19,18 @@ class ListProvidersService {
   ) {}
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    let users = await this.cacheProvider.recover<User[]>(`providers-list:${user_id}`);
+    let users = await this.cacheProvider.recover<User[]>(
+      `providers-list:${user_id}`
+    );
 
-    if(!users) {
+    if (!users) {
       users = await this.usersRepository.findAllProviders({
-        except_user_id: user_id
+        except_user_id: user_id,
       });
 
       await this.cacheProvider.save(`providers-list:${user_id}`, users);
     }
-  
+
     return users;
   }
 }
